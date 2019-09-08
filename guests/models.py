389ad68883d621a5311a0bdfa57ea23e48e15aa8
Julia <julia.kuzel@gmail.com>
2019-09-08 18:35:44 -0500

    added notes field to guests

--- guests/models.py
+++ guests/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Guest (models.Model):
 
     name = models.CharField(max_length=100)
-    additional_guests = models.TextField(max_length=1000)
+    additional_guests = models.TextField(max_length=1000, blank=True)
     total_guests = models.IntegerField(default=0)
     address = models.TextField(max_length=1000)
     phone = models.CharField(max_length=100, blank=True)
@@ -15,6 +15,7 @@ class Guest (models.Model):
     rsvp_welcome_dinner = models.IntegerField(default=0)
     rsvp_boat = models.IntegerField(default=0)
     rsvp_party = models.IntegerField(default=0)
+    notes = models.TextField(max_length=1000, blank=True)
         
     email = models.EmailField(blank=True)
     
