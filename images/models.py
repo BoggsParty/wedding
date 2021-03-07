86656e0b6cc84e82be173d4f9c407ec9fbeba05d
Julia <wedding@juliakuzel.com>
2021-03-07 10:57:41 -0600

    changed image url again

--- images/models.py
+++ images/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Image(models.Model):
     title = models.CharField(max_length=100)
-    url = models.SlugField(default='', unique=True, blank=True)
+    url = models.CharField(max_length=100, blank=True)
     image = models.ImageField()
     gallery = models.BooleanField(default=False)
     order = models.IntegerField(default=0)
